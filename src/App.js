fd3ecc45a6aec6d3c7295f2ffb1396dd7f3ae605
Patrick Rashidi <prashidi174@gmail.com>
2019-08-23 13:19:23 +0200

    Layouts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layouts/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React!</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <h1>Hello React!</h1>
+        </div>
+      </div>
+    </Router>
   );
 }
 
